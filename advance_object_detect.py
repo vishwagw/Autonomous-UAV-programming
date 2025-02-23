73e9f2ac87a4df121b2e4d687fedb01d41777e04
Vishwa GW <136871744+vishwagw@users.noreply.github.com>
2025-02-23 05:48:30 +0530

    Update advance_object_detect.py

--- advance_object_detect.py
+++ advance_object_detect.py
@@ -7,4 +7,43 @@ import imutils
 from imutils.video import VideoStream
 from imutils.video import FPS
 
+# argumen paersing:
+ap = argparse.ArgumentParser()
+ap.add_argument("-p", "--prototxt", required=True,
+	help="./models/MobileNetSSD_deploy.caffemodel")
+ap.add_argument("-m", "--model", required=True,
+	help="./models/MobileNetSSD_deploy.prototxt.txt")
+ap.add_argument("-c", "--confidence", type=float, default=0.2,
+	help="minimum probability to filter weak predictions")
+args = vars(ap.parse_args())
+
+CLASSES = ["aeroplane", "background", "bicycle", "bird", "boat",
+           "bottle", "bus", "car", "cat", "chair", "cow", "diningtable",
+           "dog", "horse", "motorbike", "person", "pottedplant", "sheep",
+           "sofa", "train", "tvmonitor"]
+
+# Assigning random colors to each of the classes
+COLORS = np.random.uniform(0, 255, size=(len(CLASSES), 3))
+
+# COLORS: a list of 21 R,G,B values, like ['101.097383   172.34857188 111.84805346'] for each label
+# length of COLORS = length of CLASSES = 21
+
+# load our serialized model
+# The model from Caffe: MobileNetSSD_deploy.prototxt.txt; MobileNetSSD_deploy.caffemodel;
+print("[INFO] loading model...")
+net = cv2.dnn.readNetFromCaffe(args["prototxt"], args["model"])
+# print(net)
+# <dnn_Net 0x128ce1310>
+
+# initialize the video stream,
+# and initialize the FPS counter
+print("[INFO] starting video stream...")
+vs = VideoStream(src=0).start()
+# warm up the camera for a couple of seconds
+time.sleep(2.0)
+
+# FPS: used to compute the (approximate) frames per second
+# Start the FPS timer
+fps = FPS().start()
+
 
