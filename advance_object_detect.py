395a4277e0c70ecb24e60ebb7e7b1c1a6c32c81c
Vishwa GW <136871744+vishwagw@users.noreply.github.com>
2025-02-23 05:48:59 +0530

    Update advance_object_detect.py

--- advance_object_detect.py
+++ advance_object_detect.py
@@ -46,4 +46,64 @@ time.sleep(2.0)
 # Start the FPS timer
 fps = FPS().start()
 
+# Consider the video stream as a series of frames. We capture each frame based on a certain FPS, and loop over each frame
+# loop over the frames from the video stream
+while True:
+	# grab the frame from the threaded video stream and resize it to have a maximum width of 400 pixels
+	# vs is the VideoStream
+	frame = vs.read()
+	frame = imutils.resize(frame, width=400)
+	print(frame.shape) # (225, 400, 3)
+	# grab the frame dimensions and convert it to a blob
+	# First 2 values are the h and w of the frame. Here h = 225 and w = 400
+	(h, w) = frame.shape[:2]
+	# Resize each frame
+	resized_image = cv2.resize(frame, (300, 300))
+	# Creating the blob
+	# The function:
+	# blob = cv2.dnn.blobFromImage(image, scalefactor=1.0, size, mean, swapRB=True)
+	# image: the input image we want to preprocess before passing it through our deep neural network for classification
+	# mean:
+	# scalefactor: After we perform mean subtraction we can optionally scale our images by some factor. Default = 1.0
+	# scalefactor  should be 1/sigma as we're actually multiplying the input channels (after mean subtraction) by scalefactor (Here 1/127.5)
+	# swapRB : OpenCV assumes images are in BGR channel order; however, the 'mean' value assumes we are using RGB order.
+	# To resolve this discrepancy we can swap the R and B channels in image  by setting this value to 'True'
+	# By default OpenCV performs this channel swapping for us.
+
+	blob = cv2.dnn.blobFromImage(resized_image, (1/127.5), (300, 300), 127.5, swapRB=True)
+	# print(blob.shape) # (1, 3, 300, 300)
+	# pass the blob through the network and obtain the predictions and predictions
+	net.setInput(blob) # net = cv2.dnn.readNetFromCaffe(args["prototxt"], args["model"])
+	# Predictions:
+	predictions = net.forward()
+
+	# loop over the predictions
+	for i in np.arange(0, predictions.shape[2]):
+		# extract the confidence (i.e., probability) associated with the prediction
+		# predictions.shape[2] = 100 here
+		confidence = predictions[0, 0, i, 2]
+		# Filter out predictions lesser than the minimum confidence level
+		# Here, we set the default confidence as 0.2. Anything lesser than 0.2 will be filtered
+		if confidence > args["confidence"]:
+			# extract the index of the class label from the 'predictions'
+			# idx is the index of the class label
+			# E.g. for person, idx = 15, for chair, idx = 9, etc.
+			idx = int(predictions[0, 0, i, 1])
+			# then compute the (x, y)-coordinates of the bounding box for the object
+			box = predictions[0, 0, i, 3:7] * np.array([w, h, w, h])
+			# Example, box = [130.9669733   76.75442174 393.03834438 224.03566539]
+			# Convert them to integers: 130 76 393 224
+			(startX, startY, endX, endY) = box.astype("int")
+
+			# Get the label with the confidence score
+			label = "{}: {:.2f}%".format(CLASSES[idx], confidence * 100)
+			print("Object detected: ", label)
+			# Draw a rectangle across the boundary of the object
+			cv2.rectangle(frame, (startX, startY), (endX, endY),
+				COLORS[idx], 2)
+			y = startY - 15 if startY - 15 > 15 else startY + 15
+			# Put a text outside the rectangular detection
+			# Choose the font of your choice: FONT_HERSHEY_SIMPLEX, FONT_HERSHEY_PLAIN, FONT_HERSHEY_DUPLEX, FONT_HERSHEY_COMPLEX, FONT_HERSHEY_SCRIPT_COMPLEX, FONT_ITALIC, etc.
+			cv2.putText(frame, label, (startX, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, COLORS[idx], 2)
+
 
