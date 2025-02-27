ee886e6fea2859bf8ba357ad039161a94d4ac97e
Vishwa GW <136871744+vishwagw@users.noreply.github.com>
2025-02-28 03:59:34 +0530

    Update track.py

--- road_tracking_model/track.py
+++ road_tracking_model/track.py
@@ -3,3 +3,16 @@ import cv2
 import supervision as sv
 import numpy as np
 from ultralytics import YOLO
+
+# import pretrained model:
+model = YOLO('./models/yolov8n.pt')
+
+# vid path:
+vid = './input/in1.mp4'
+cap = cv2.VideoCapture(vid)
+# Load YOLOv8 model
+model = YOLO("yolov8n.pt")  # Replace with custom-trained model for road detection
+
+# Start video capture
+cap = cv2.VideoCapture(0)  # Change to drone camera sourc
+
