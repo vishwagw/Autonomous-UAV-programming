7e37fc9181ffaa98d05da1743356079f92307db8
Vishwa GW <136871744+vishwagw@users.noreply.github.com>
2025-03-14 19:37:45 +0530

    Update gps_navigation.py

--- autonomous movements/gps_navigation.py
+++ autonomous movements/gps_navigation.py
@@ -1,2 +1,39 @@
 # gps based navigation for autonomous movements:
 # code structure is only for movements, excluding obstacle avoidance
+from dronekit import connect, VehicleMode, LocationGlobalRelative
+import time
+from pymavlink import mavutil
+
+# takeoff:
+def arm_and_takeoff(target_altitude):
+    """
+    Arms the drone and takes off to the target altitude.
+    """
+    print("Arming motors...")
+    autumn1.mode = VehicleMode("GUIDED")
+    autumn1.armed = True
+
+    while not autumn1.armed:
+        print("Waiting for arming...")
+        time.sleep(1)
+
+    print("Taking off...")
+    autumn1.simple_takeoff(target_altitude)
+
+    while True:
+        print(f"Altitude: {autumn1.location.global_relative_frame.alt:.2f}m")
+        if autumn1.location.global_relative_frame.alt >= target_altitude * 0.95:
+            print("Target altitude reached!")
+            break
+        time.sleep(1)
+# navigate to a specific loaction:
+def goto_loc(lat, lon, alt):
+    print(f"Navigating to: Lat={lat}, Lon={lon}, Alt={alt}m")
+    point = LocationGlobalRelative(lat, lon, alt)
+    autumn1.simple_goto(point)
+
+# landing:
+def land():
+    print("vehicle is landing..")
+    autumn1.mode = VehicleMode("LAND")
+
